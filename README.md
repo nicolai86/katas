a05b4a6507ce8c98ce75b1b67a6ff8632c502a5a
Raphael Randschau <nicolai86@me.com>
2015-10-04 21:21:43 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Katas
+
+Solutions to common coding kata plattforms like exercism.io and project euler.
--- exercism/go/leap/leap.go
+++ exercism/go/leap/leap.go
@@ -1,5 +1,9 @@
+/*
+Package leap includes predicates useful when working with dates
+*/
 package leap
 
+// IsLeapYear returns true if a given year is a leap year
 func IsLeapYear(year int) bool {
 	var isLeap = year%4 == 0
 	var isCentury = year%100 == 0
